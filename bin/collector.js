2ffc913e15bd272dcf190086f04842868af5702b
Florian Traverse <florian.traverse@gmail.com>
2012-07-19 20:14:51 +0200

    made the server more like a http server so we can use it as test or use things like distribute on top of it

--- bin/collector.js
+++ bin/collector.js
@@ -2,8 +2,6 @@ var options = require("./collector-config"),
     cube = require("../"),
     server = cube.server(options);
 
-server.register = function(db, endpoints) {
-  cube.collector.register(db, endpoints);
-};
-
-server.start();
+server
+  .use(cube.collector.register)
+  .start();
--- bin/evaluator.js
+++ bin/evaluator.js
@@ -2,8 +2,6 @@ var options = require("./evaluator-config"),
     cube = require("../"),
     server = cube.server(options);
 
-server.register = function(db, endpoints) {
-  cube.evaluator.register(db, endpoints);
-};
-
-server.start();
+server
+  .use(cube.evaluator.register)
+  .start()
